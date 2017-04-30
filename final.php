3a193088ce44a791e4ad4ec1288e7bf0898e3707
Christian Gauthier <cgauthi2@jhu.edu>
2017-04-30 11:42:50 -0400

    Add floating search and improve exposition.

--- final.php
+++ final.php
@@ -20,9 +20,9 @@ $result = mysqli_query($connection, $sql) or die ("Error " . mysqli_error($conne
 		<img id="byline" src="./img/testbanner2.jpg">
 		<div id="content">
 		<h2>Gene-Disease Association Tool</h2>	
-			<p>Through the curbing influences of evolution, the human genome has created endless permutations of itself - sampling small changes which can lead to drastic (or not so drastic) results.  The ~3.2 billion nucleotides that are packed within each cell in our bodies are constantly in flux.  They experiment with, edit, and correct themselves silently within.  Occasionally, these alterations will lead to the manifestation of genetic disease.
+			<p>Through the curbing influences of evolution, the human genome has created endless permutations of itself - sampling small changes which can lead to drastic (or not so drastic) results.  The ~3.2 billion nucleotides that are packed within each cell in our bodies are constantly in flux.  They are added, removed, transformed, and silently corrected throughout the genome.  This variability means that each gene has the potential to mutate and manifest an aberrant phenotype in the form of genetic disease.
 			</p>
-			<p> With advances in molecular biology and bioinformatics technology, we are now able to accurately track down which changes lead to a given disease phenotype.  Every gene has the potential to subtly shift and create an outcome out of tune with a healthy organism.  This knowledge is constantly being updated and utilized to generate new therapies which may help cure these diseases.  So I ask you...
+			<p> With advances in molecular biology and bioinformatics technology, we are now able to accurately track down which changes lead to a given disease phenotype.  This knowledge is constantly being updated and stored in massive data repositories for use in research applications.  Scientists can utilize this information to generate new therapies which may help cure these diseases.  So I ask you...
 			</p>   
 		<form action="/cgi-bin/gene_search.cgi" method="POST">
 			<label for='gene_name'>Which gene are you curious about?</label>
